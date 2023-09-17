95edd061454b4614f3e105f2c50f165d3c75e68b
Lindsey Bartges <lrbartges@gmail.com>
2023-09-17 14:27:06 -0700

    complete assignment

---
+++ index.js
@@ -0,0 +1,57 @@
+function add() {
+return "something";
+}
+function subtract() {
+return "something";
+}
+function multiply() {
+return "something";
+}
+function divide() {
+return "something";
+}
+function add(a, b) {
+    return "something";
+}
+function add (a, b) {
+    return a+b;
+}
+function subtract (a, b) {
+    return "something";
+}
+function subtract(a, b) {
+    return a-b;
+}
+function multiply(a, b) {
+    return "something"
+}
+function multiply (a, b) {
+    return a*b;
+}
+function divide (a, b) {
+    return "something"
+}
+function divide (a, b) {
+    return a/b;
+    parseFloat (2.1800554016620497)
+}
+function increment(n) {
+    const result= n++;
+    return result;
+}
+function increment(n) {
+    return n+1;
+}
+function decrement(n) {
+    const result= n--
+    return result;
+}
+function decrement(n) {
+    return n-1;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
